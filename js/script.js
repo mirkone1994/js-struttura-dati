b5756bdac4911ef55578792fde21cc6e4cb210e0
mirkone1994 <mirkochiappori@gmail.com>
2021-07-23 18:10:43 +0200

    end card 1

--- js/script.js
+++ js/script.js
@@ -48,14 +48,49 @@ const card = {
 
 console.log(card);
 
+let abilitiesContent = "Nessuna abilità";
+if (card.abilities.length) {
+    abilitiesContent = "<ul>";
+    for (let i = 0; i < card.abilities.length; i++) {
+        const currentABility = card.abilities[i];
+        abilitiesContent += "---";
+        abilitiesContent += `<li>Descrizione: ${currentABility.description}</li>`;
+        abilitiesContent += `<li>Costo di lancio: ${currentABility.launchCost.join(", ")}</li>`;
+        abilitiesContent += "</ul>";
+    }
+}
+
 const cardSection = document.getElementById("cards");
+const cardSection2 = document.getElementById("cards2");
+const subType = card.subType ? `- ${card.subType}` : " ";
 let cardTemplate = `
 <ul class "card">
     <li>Id: ${card.id}</li>
     <li>Nome: ${card.name}</li>
     <li>Costo di lancio: ${card.launchCost.join(", ")}</li>
     <li>Costo cmc: ${card.cmc}</li>
-    
-`
+    <li>Tipo di carta: ${card.cardType} ${subType}</li>
+    <li>Espansione: 
+        <ul>
+            <li>Ristampa: ${card.expansion.reprintId}</li>
+            <li>Nome: ${card.expansion.name}</li>
+            <li>Rarità: ${card.expansion.rarity}</li>
+            <li>Posizione numero: ${card.expansion.collectionNr}/${card.expansion.totalCard}</li>
+        </ul>
+    </li>
+    <li>Testo di Colore: ${card.flavorText.quote} - ${card.flavorText.author}</li>
+    <li>Abilità: ${abilitiesContent}</li>
+    <li>Costituzione: ${card.constitution}</li>
+    <li>Forza: ${card.strength}</li>
+    <li>Colore bordo: ${card.borderColor}</li>
+    <li>
+        <ul>
+            <li>Autore: ${card.illustration.autohr.source} (id: ${card.illustration.autohr.id})</li>
+            <li>Link illustrazione: ${card.illustration.source}</li>
+        </ul>
+    </li>
+</ul>
+`;
 cardSection.innerHTML = cardTemplate;
+cardSection2.innerHTML = cardTemplate;
 
